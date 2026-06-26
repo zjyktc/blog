576091969bf49d38a485f6682aae52bf3ca20f11
ykfy1 <yktcyfy@foxmail.com>
2026-06-26 16:30:12 +0800

    Update backgroundWallpaper.ts

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -43,22 +43,21 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/DesktopWallpaper/zhuti.jpg",
+			"assets/images/DesktopWallpaper/zhuti.jpg",
+			"assets/images/DesktopWallpaper/zhuti.jpg",
+			"assets/images/DesktopWallpaper/zhuti.jpg",
+			"assets/images/DesktopWallpaper/zhuti.jpg",
+			"assets/images/DesktopWallpaper/zhuti.jpg",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
+			"assets/images/MobileWallpaper/zhuti.jpg",
+			"assets/images/MobileWallpaper/zhuti.jpg",
+			"assets/images/MobileWallpaper/zhuti.jpg",
+			"assets/images/MobileWallpaper/zhuti.jpg",
+			"assets/images/MobileWallpaper/zhuti.jpg",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组）
